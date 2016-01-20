9499b014122763e9fa1861709f885301ea57e1ed
Sander van den Berg <svandenberg@ebay.com>
2016-01-20 22:21:34 +0100

    added set test

--- datamodel/activity_test.go
+++ datamodel/activity_test.go
@@ -41,6 +41,66 @@ func TestConsideredEqualOutsideOfDelta(t *testing.T) {
 	}
 }
 
+func TestApproxSetDifference(t *testing.T) {
+	set1 := NewActivitySet()
+	activity1 := CreateActivity()
+	activity1.Name = "Activity1"
+	duration := 10000000
+	activity1.StartTime = int(time.Now().Unix() - int64(duration))
+	activity1.Duration = duration
+	set1.Add(*activity1)
+
+	activity2 := CreateActivity()
+	activity2.Name = "Activity2"
+	activity2.StartTime = int(time.Now().Unix())
+	activity2.Duration = duration
+	set1.Add(*activity2)
+
+	set2 := NewActivitySet()
+	set2.Add(*activity2)
+
+	activity3 := CreateActivity()
+	activity3.Name = "Activity3"
+	activity3.StartTime = int(time.Now().Unix())
+	activity3.Duration = duration
+	set2.Add(*activity3)
+
+	//{1,2}\{2,3} => {1}
+	resultSet := set1.ApproxSubtract(set2)
+
+	if resultSet.NumElements() != 1 || !resultSet.ApproxContains(*activity1) {
+		t.Error(fmt.Sprintf("%d is not equal to 1", len(resultSet.slice)))
+	}
+}
+
+func TestSetDifference(t *testing.T) {
+	set1 := NewActivitySet()
+	activity1 := CreateActivity()
+	activity1.Name = "Activity1"
+	activity1.StartTime = int(time.Now().Unix())
+	set1.Add(*activity1)
+
+	activity2 := CreateActivity()
+	activity2.Name = "Activity2"
+	activity2.StartTime = int(time.Now().Unix())
+	set1.Add(*activity2)
+
+	set2 := NewActivitySet()
+	set2.Add(*activity2)
+
+	activity3 := CreateActivity()
+	activity3.Name = "Activity3"
+	activity3.StartTime = int(time.Now().Unix())
+	set2.Add(*activity3)
+
+	//{1,2}\{2,3} => {1}
+	resultSet := set1.Subtract(set2)
+
+	if resultSet.NumElements() != 1 || !resultSet.Contains(*activity1) {
+		t.Error(fmt.Sprintf("%d is not equal to 1", len(resultSet.slice)))
+	}
+}
+
 func TestString(t *testing.T) {
 	now := time.Now()
 	nowString := now.Format("02-01-2006 15:04 MST")
@@ -49,7 +109,7 @@ func TestString(t *testing.T) {
 	activity.StartTime = int(now.Unix())
 
 	res := fmt.Sprintf("%s", activity)
-	if res != fmt.Sprintf("%s test-act", nowString) {
-		t.Error(fmt.Sprintf("%s is not equal to \"%s test-act\"", res, nowString))
+	if res != fmt.Sprintf("Activity: test-act, time: %s,  GPS: []..., HR: []...", nowString) {
+		t.Error(fmt.Sprintf("%s is not equal to \"Activity: test-act, time: %s,  GPS: []..., HR: []...\"", res, nowString))
 	}
 }
