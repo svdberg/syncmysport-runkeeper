782bc32e0acdb40cc68870701cc0c1e128dbd073
Sander van den Berg <svandenberg@ebay.com>
2016-09-12 15:32:26 +0200

    First attempt to wire active users with jQuery

--- api/static/js/active-users.js
+++ api/static/js/active-users.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     var showData = $('#active-users');
     $.getJSON('http://www.syncmysport.com/users/count', function (data) {
       console.log(data);
+      showData.textContent="Active users: " + data["active-users"]
     });
 });
